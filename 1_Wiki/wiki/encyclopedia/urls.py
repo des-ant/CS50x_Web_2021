3eb9b6facd4eb0d4f6357e4795a5a55ff088a1ca
des-ant <57375593+des-ant@users.noreply.github.com>
2021-07-15 15:46:28 +1000

    implemented random page functionality

--- 1_Wiki/wiki/encyclopedia/urls.py
+++ 1_Wiki/wiki/encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     # Pass title to wiki function to read markdown file
-    path("wiki/<str:title>", views.wiki, name="wiki")
+    path("wiki/<str:title>", views.wiki, name="wiki"),
+    # Url for random encyclopedia entry
+    path("random/", views.random_entry, name="random")
 ]
--- 1_Wiki/wiki/encyclopedia/views.py
+++ 1_Wiki/wiki/encyclopedia/views.py
@@ -1,3 +1,4 @@
+from random import choice
 from markdown2 import Markdown
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -10,6 +11,7 @@ def index(request):
         "entries": util.list_entries()
     })
 
+# Function to render encyclopedia entries to html page
 def wiki(request, title):
     # Read markdown file
     md_file = util.get_entry(title)
@@ -22,3 +24,9 @@ def wiki(request, title):
     return render(request, "encyclopedia/entry.html", {
         "entry": Markdown().convert(md_file)
     })
+
+# Function to take user to random encyclopedia entry
+def random_entry(request):
+    # Get random encyclopedia entry
+    random_entry = choice(util.list_entries())
+    return wiki(request, random_entry)
