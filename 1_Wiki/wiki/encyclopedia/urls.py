3974f5a9d9ab512f6981bc3fd3ef73f959df8f65
des-ant <57375593+des-ant@users.noreply.github.com>
2021-07-15 18:20:47 +1000

    user can create new page

--- 1_Wiki/wiki/encyclopedia/urls.py
+++ 1_Wiki/wiki/encyclopedia/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     # Pass title to wiki function to read markdown file
     path("wiki/<str:title>", views.wiki, name="wiki"),
     # Url for random encyclopedia entry
-    path("random/", views.random_entry, name="random")
+    path("random/", views.random_entry, name="random"),
+    # Url for creating new encyclopedia entry
+    path("newentry/", views.new_entry, name="newentry")
 ]
--- 1_Wiki/wiki/encyclopedia/views.py
+++ 1_Wiki/wiki/encyclopedia/views.py
@@ -1,7 +1,10 @@
 from random import choice
 from markdown2 import Markdown
-from django.http import HttpResponse
+from django import forms
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from . import util
 
@@ -30,3 +33,40 @@ def random_entry(request):
     # Get random encyclopedia entry
     random_entry = choice(util.list_entries())
     return wiki(request, random_entry)
+
+# Django form, inherits from forms.form
+class new_entry_form(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(widget=forms.Textarea, label="Content")
+
+# Function to create new entry
+def new_entry(request):
+    # If new entry being submitted
+    if request.method == "POST":
+        form = new_entry_form(request.POST)
+        # Check for valid input
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            # Add markdown title to content
+            content = f"# {title}\n{content}"
+            # If entry exists, present error message
+            if util.get_entry(title):
+                messages.error(request, "Error: Page already exists")
+                # Send back existing form data
+                return render(request, "encyclopedia/newentry.html", {
+                    "form": form
+                })
+            # Save entry to disk and take user to new entry's page
+            else:
+                util.save_entry(title, content)
+                return HttpResponseRedirect(reverse("encyclopedia:wiki", args=[title]))
+        # Invalid input, send back existing form data
+        else:
+            return render(request, "encyclopedia/newentry.html", {
+                "form": form
+            })
+    # Render empty form if received get request
+    return render(request, "encyclopedia/newentry.html", {
+        "form": new_entry_form()
+    })
