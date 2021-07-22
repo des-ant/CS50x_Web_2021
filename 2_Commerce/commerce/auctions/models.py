a58368665915163a431095ac33ccfd28aa530266
des-ant <57375593+des-ant@users.noreply.github.com>
2021-07-22 12:57:55 +1000

    updated models, removed incorrect foreignkeys

--- 2_Commerce/commerce/auctions/models.py
+++ 2_Commerce/commerce/auctions/models.py
@@ -3,49 +3,10 @@ from django.db import models
 
 
 class User(AbstractUser):
-    listings = models.ForeignKey(Listing, blank=True, related_name="creator")
-    bids = models.ForeignKey(Bid, blank=True, related_name="bidder")
-    comments = models.ForeignKey(Comment, blank=True, related_name="author")
-    watchlist = models.ManyToManyField(Listing, blank=True, related_name="watchers")
+    pass
 
     def __str__(self):
-        return f"{self.id} {self.listings} {self.bids} {self.comments} {self.watchlist}"
-
-# Model for auction listing
-class Listing(models.Model):
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
-    title = models.CharField(max_length=64)
-    description = models.TextField()
-    image = models.ImageField()
-    category = models.ForeignKey(Category, related_name="catgory_items")
-    date_created = models.DateTimeField()
-    is_active = models.BooleanField()
-    price = models.DecimalField(decimal_places=2)
-    bids = models.ForeignKey(Bid, blank=True, related_name="bid_listing")
-    
-    def __str__(self):
-        return f"{self.id} {self.creator} {self.title} {self.description} {self.image}"
-        f"{self.category} {self.date_created} {self.is_active} {self.bids}"
-
-# Model for bids
-class Bid(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bids")
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_bids")
-    price = models.DecimalField(decimal_places=2)
-    date = models.DateTimeField()
-
-    def __str__(self):
-        return f"{self.id} {self.user} {self.listing} {self.price} {self.date}"
-
-# Model for comments made on auction listings
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_comments")
-    comment = models.TextField()
-    date = models.DateTimeField()
-
-    def __str__(self):
-        return f"{self.id} {self.listing} {self.comment} {self.date}"
+        return f"{self.id}"
 
 # Allow listings to be sorted by category
 class Category(models.Model):
@@ -53,3 +14,40 @@ class Category(models.Model):
 
     def __str__(self):
         return f"{self.category}"
+
+# Model for auction listing
+class Listing(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    image = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="catgory_items")
+    date_created = models.DateTimeField()
+    is_active = models.BooleanField()
+    price = models.DecimalField(max_digits=19, decimal_places=2)
+    watchers = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="watchlist")
+    
+    def __str__(self):
+        return f"{self.id} {self.creator} {self.title} {self.description} {self.image}"
+        f"{self.category} {self.date_created} {self.is_active} {self.watchers}"
+
+# Model for bids
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_bids")
+    price = models.DecimalField(max_digits=19, decimal_places=2)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.id} {self.user} {self.listing} {self.price} {self.date}"
+
+# Model for comments made on auction listings
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_comments")
+    comment = models.TextField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.id} {self.listing} {self.comment} {self.date}"
+
