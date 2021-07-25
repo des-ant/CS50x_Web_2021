754318a9a41f50502ba04886c68d06a942a7571e
des-ant <57375593+des-ant@users.noreply.github.com>
2021-07-25 16:49:53 +1000

    boostrap 4 styling modelForm

--- 2_Commerce/commerce/auctions/forms.py
+++ 2_Commerce/commerce/auctions/forms.py
@@ -1,11 +1,18 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Category, Listing
 
 
-class NewListingForm(ModelForm):
+class NewListingForm(forms.ModelForm):
     """
     New listing form from listing model
     """
     class Meta:
         model = Listing
         fields = ['title', 'description', 'price', 'image', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={"class": "form-control"}),
+            'description': forms.Textarea(attrs={"class": "form-control"}),
+            'price': forms.NumberInput(attrs={"class": "form-control"}),
+            'image': forms.FileInput(attrs={"class": "form-control"}),
+            'category': forms.Select(attrs={"class": "form-control"})
+        }
